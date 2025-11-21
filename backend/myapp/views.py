5eeccd042e4bd0c7922affed93d7d38721f35958
DanTar <tarasovdanil2207@gmail.com>
2025-11-21 14:05:56 +0400

    update configure

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
 
 # Create your views here.
+class Auth:
+    @api_view(['POST'])
+    def r(request):
+        return(HttpResponse(1))
